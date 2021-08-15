008131ee4ae911a17f28ec2393d784d4b9f67cdd
ZelKnow <shenghuang@stu.pku.edu.cn>
2021-08-15 08:42:37 +0800

    ✨ feat(card): init plugin card

--- src/plugins/hearthstone_card/__init__.py
+++ src/plugins/hearthstone_card/__init__.py
@@ -8,3 +8,78 @@
 @Github        : https://github.com/ZelKnow
 """
 __author__ = "ZelKnow"
+
+from nonebot import get_driver
+from nonebot import on_command
+from nonebot.typing import T_State
+from nonebot.rule import to_me
+from nonebot.adapters import Bot, Event
+from .card_handler import CardHandler
+from nonebot.adapters.cqhttp.message import MessageSegment
+from .utils import supported_langs
+from nonebot.message import handle_event
+cardhandler = CardHandler()
+global_config = get_driver().config
+
+hearthstone_card = on_command("card", aliases={"c", "C", "CARD", "Card"},
+                              state={"type": "card"}, priority=0)
+hearthstone_tags = on_command("tags", aliases={"t", "T", "TAGS", "Tags", "tag"},
+                              state={"type": "tags"}, priority=1)
+max_response = global_config.max_response
+
+
+@hearthstone_tags.handle()
+@hearthstone_card.handle()
+async def handle_frist_receive(bot: Bot, event: Event, state: T_State):
+    parts = str(event.get_message()).split(" / ")
+    state['terms'] = parts[0].strip().lower().split()
+    state['lang'] = 'zhCN'
+    if len(parts) == 2:
+        for part in parts[1].split():
+            if len(part) == 4:
+                lang = part[0:2].lower()+part[2:4].upper()
+                if lang not in supported_langs:
+                    bot.finish("不支持%s语言，请输入正确格式的语言代码如enUS、jaJP等。" % lang)
+                state['lang'] = lang
+            elif part.lower() == 'ori':
+                state['is_ori'] = True
+    state['cards'], state['hint'] = cardhandler.first_handle(
+        state['terms'], max_response)
+    print(state['cards'])
+    if len(state['cards']) == 1:
+        msg = hscard_msg(state['cards'][0], state['lang'], state["type"])
+        await bot.send(event, msg)
+    elif len(state['cards']) == 0:
+        await hearthstone_card.finish(state['hint'])
+    else:
+        await hearthstone_card.pause(state['hint'])
+
+
+@hearthstone_tags.handle()
+@hearthstone_card.handle()
+async def handle_receive(bot: Bot, event: Event, state: T_State):
+    raw = str(event.get_message()).strip()
+    if raw.startswith('\\') or raw.startswith('/'):
+        try:
+            card = state['cards'][int(raw[1:])-1]
+        except Exception as e:
+            await hearthstone_card.reject("卡牌编号输入有误，请重新输入")
+        msg = hscard_msg(card, state['lang'], state["type"])
+        await bot.send(event, msg)
+    elif raw.isdigit():
+        page = int(raw)
+        state['hint'] = cardhandler.second_handle(
+            state['cards'], page, max_response)
+        await hearthstone_card.reject(state['hint'])
+    else:
+        await handle_event(bot, event)
+        await hearthstone_card.finish()
+
+
+def hscard_msg(card, lang, type):
+    if type == "card":
+        url = cardhandler.get_pic(card, lang)
+        return MessageSegment.image(url)
+    elif type == "tags":
+        tags = cardhandler.get_tags(card, lang)
+        return tags
---
+++ src/plugins/hearthstone_card/card_handler.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File          : card_handler.py
+@Description   : Handle Hearthstone card.
+@Date          : 2021/08/12 22:40:39
+@Author        : ZelKnow
+@Github        : https://github.com/ZelKnow
+"""
+__author__ = "ZelKnow"
+
+from hearthstone import cardxml
+from hearthstone.cardxml import CardXML
+from hearthstone.enums import CardType, GameTag, Race, Rarity, MultiClassGroup
+from .utils import set_map, class_map, multiclass_map, type_map, rarity_map
+
+
+db, _ = cardxml.load()
+
+
+def loc_name(self, locale):
+    return self.strings[GameTag.CARDNAME][locale]
+
+
+def set_name(self, locale):
+    return self.strings[GameTag.CARD_SET][locale]
+
+
+def loc_text(self, locale):
+    return self.strings[GameTag.CARDTEXT_INHAND][locale]
+
+
+def loc_flavor(self, locale):
+    return self.strings[GameTag.FLAVORTEXT][locale]
+
+
+CardXML.loc_name = loc_name
+CardXML.loc_text = loc_text
+CardXML.loc_flavor = loc_flavor
+
+
+class CardHandler():
+    def __init__(self):
+        self.db = {}
+        for key in db.keys():
+            self.db[key.lower()] = db[key]
+
+    def first_handle(self, terms, max_response):
+        page = 1
+        cards = []
+        for card in db.values():
+            if card.type == CardType.ENCHANTMENT:
+                continue
+            card_name = card.loc_name('zhCN').lower()
+            if all([term in card_name for term in terms]):
+                cards.append(card)
+        num_cards = len(cards)
+        if num_cards == 0:
+            hint = "Card not found"
+        elif num_cards == 1:
+            hint = ""
+        else:
+            hint = self.second_handle(cards, 1, max_response)
+        return cards, hint
+
+    def second_handle(self, cards, page, max_response):
+        num_cards = len(cards)
+        page_size = min(max_response, num_cards)
+        page_count = int(num_cards / page_size)
+        page = min(page_count, max(1, page))
+        offset = (page-1) * page_size
+        page_hint = (
+            '查询到%d个卡牌，当前页数[%d/%d]，直接输入数字进行翻页\n'
+            % (num_cards, page, page_count)
+        )
+        hint = page_hint + "\n".join(
+            self.stringify_card(cards[i], i + 1)
+            for i in range(offset, min(offset + page_size, num_cards))
+        )
+        return hint
+
+    def stringify_card(self, card, index):
+        collectible = "可收藏" if card.collectible else "不可收藏"
+        card_class = multiclass_map[card.multi_class_group.name] \
+            if card.multi_class_group != MultiClassGroup.INVALID else class_map[card.card_class.name]
+        cost = "%d费" % card.cost
+        card_type = type_map[card.type.name]
+        name = card.loc_name('zhCN')
+        card_set = set_map[card.card_set.name]
+        return (
+            "\\%d：%s，%s%s%s，%s，%s"
+            % (index, name, cost, card_class, card_type, collectible, card_set)
+        )
+
+    def get_pic(self, card, lang):
+        return "http://art.hearthstonejson.com/v1/render/latest/%s/512x/%s.png" % (lang, card.id)
+
+    def get_tags(self, card, lang):
+        name = "名称：%s" % card.loc_name(lang)
+        card_id = "\nid：%s" % card.id
+        health = card.durability if card.type == CardType.WEAPON else card.health
+        stats = "\n身材：%s费%s/%s" % (card.cost, card.atk,
+                                   health) if card.atk + health > 0 else ""
+        race = "\n种族：%s" % card.race.name.title() if card.race != Race.INVALID else ""
+        rarity = "\n稀有度：%s" % rarity_map[card.rarity.name] if card.rarity != Rarity.INVALID else ""
+        text = "\n" + card.loc_text(lang) if len(card.description) else ""
+        flavor = "\n卡牌趣文：" + \
+            card.loc_flavor(lang) if len(card.flavortext) else ""
+        card_class = "\n职业：%s" % (multiclass_map[card.multi_class_group.name]
+                                  if card.multi_class_group != MultiClassGroup.INVALID else class_map[card.card_class.name])
+        card_set = "\n扩展包：%s" % set_map[card.card_set.name]
+        collectible = "\n可否收藏：%s" % ("是" if card.collectible else "否")
+        tags = name + card_id + text + flavor + card_class + race + card_set + stats \
+            + rarity + collectible
+        return tags
